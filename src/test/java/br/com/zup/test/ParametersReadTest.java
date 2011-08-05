a79127c551c297c8844131a03690d13c6859b1eb
Abner Carleto <abnercarleto@ymail.com>
2011-08-05 15:44:26 -0300

    criado projeto de plugin usando achetype do maven

---
+++ src/test/java/br/com/zup/test/ParametersReadTest.java
@@ -0,0 +1,19 @@
+package br.com.zup.test;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParametersReadTest extends AbstractMojoTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Test
+	public void ShoulReadPackageScanner() {
+
+	}
+
+}
