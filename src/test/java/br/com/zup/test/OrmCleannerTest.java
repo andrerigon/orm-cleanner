8852ffb927b0d67ca04cc92e94bd24add9fd3828
Abner Carleto <abnercarleto@ymail.com>
2011-08-08 15:27:22 -0300

    Criado test do metodo execute

--- src/test/java/br/com/zup/test/OrmCleannerTest.java
+++ src/test/java/br/com/zup/test/OrmCleannerTest.java
@@ -2,6 +2,7 @@ package br.com.zup.test;
 
 import java.io.File;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.codehaus.plexus.PlexusTestCase;
 
 import br.com.zup.OrmCleanner;
@@ -12,16 +13,37 @@ public class OrmCleannerTest extends PlexusTestCase {
 
 	private OrmCleanner mojo;
 	
+	private static File outputDirectory;
+	
+	static {
+		outputDirectory = new File(getBasedir(), DEFAULT_OUTPUT_DIRECTORY);
+	}
+	
 	public void setUp() throws Exception {
 		super.setUp();
 		
 		mojo = (OrmCleanner) lookup(OrmCleanner.ROLE);
 		
 		assertNotNull(mojo);
+		
+		setParameters();
+	}
+	
+	public void tearDown() throws Exception {
+		mojo = null;
+	}
+	
+	public void testExecute() throws MojoExecutionException {
+		mojo.execute();
+		
+		File touch = new File(outputDirectory, "touch.txt");
+		
+		if (!touch.exists())
+			fail("touch not write");
 	}
 
 	public void setParameters() {
-		mojo.setOutputDirectory(new File(getBasedir(), DEFAULT_OUTPUT_DIRECTORY));
+		mojo.setOutputDirectory(outputDirectory);
 	}
 
 }
