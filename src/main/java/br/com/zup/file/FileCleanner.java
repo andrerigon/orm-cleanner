a0d65407d595839b7d570ed1f0cf30d4a62cc432
Abner Carleto <abnercarleto@ymail.com>
2011-08-09 15:00:14 -0300

    Criado codigo que verifica se o arquivo tem a annotation @Entity

--- src/main/java/br/com/zup/file/FileCleanner.java
+++ src/main/java/br/com/zup/file/FileCleanner.java
@@ -1,7 +1,12 @@
 package br.com.zup.file;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileCleanner {
 
@@ -12,10 +17,23 @@ public class FileCleanner {
 		this.javaClass = javaClass;
 	}
 
-	public boolean isEntity() {
-		// TODO: verificar se tem annotation @Entity
+	public boolean isEntity() throws IOException {
+		FileReader entity = new FileReader(javaClass);
+		BufferedReader reader = new BufferedReader(entity);
+		
+		String line;
+		Pattern regex = Pattern.compile(LinesRemove.ANNOTATION_ENTITY.lineRegex());  
+		while ( (line = reader.readLine()) != null ) {
+			Matcher matcher = regex.matcher(line);
+			if (matcher.find())
+				return true;
+		}
 		return false;
 	}
-	
+
+	public FileReader clean() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
--- src/test/java/br/com/zup/file/test/FileCleannerTest.java
+++ src/test/java/br/com/zup/file/test/FileCleannerTest.java
@@ -1,29 +1,56 @@
 package br.com.zup.file.test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
 
 import br.com.zup.file.FileCleanner;
 
-
 public class FileCleannerTest {
 
-	private static final String ENTITY_CLASS = "/target/test-classes/unit/orm-cleanner-test/src/main/java/br/com/ctbc/model/Agent.java";
-	
+	private static final String ENTITY_CLASS = "Agent.java";
+	private static final String NO_ENTITY_CLASS = "AgentClean.java";
+
+	private static final String LOCATION_CLASSES = "/target/test-classes/unit/orm-cleanner-test/src/main/java/br/com/ctbc/model/";
+
 	private static final String localDir;
-	
+
 	static {
 		localDir = System.getProperty("user.dir");
 	}
-	
+
 	@Test
-	public void shouldFileIsEntity() {
-		File entityFile = new File(localDir + ENTITY_CLASS);
+	public void shouldFileIsEntity() throws IOException {
+		File entityFile = new File(localDir + LOCATION_CLASSES + ENTITY_CLASS);
+
+		FileCleanner cleanner = new FileCleanner(entityFile);
+		assertTrue(cleanner.isEntity());
+	}
+
+	@Test
+	public void shouldFileNotIsEntity() throws IOException {
+		File noEntityFile = new File(localDir + LOCATION_CLASSES + NO_ENTITY_CLASS);
+
+		FileCleanner cleanner = new FileCleanner(noEntityFile);
+		assertFalse(cleanner.isEntity());
+	}
+
+	@Test
+	public void sholdClean() throws Exception {
+		File entityFile = new File(localDir + LOCATION_CLASSES + ENTITY_CLASS);
+		
+		File noEntityFile = new File(localDir + LOCATION_CLASSES + NO_ENTITY_CLASS);
+		FileReader noEntityReader = new FileReader(noEntityFile);
 		
 		FileCleanner cleanner = new FileCleanner(entityFile);
-		assertTrue( cleanner.isEntity() );
+		FileReader entityReader = cleanner.clean();
+		
+		assertTrue( entityReader.equals(noEntityReader) );
 	}
+
 }
