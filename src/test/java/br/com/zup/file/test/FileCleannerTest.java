46087908f99a5fa52756865e8bfb42efaa9042db
Abner Carleto <abnercarleto@ymail.com>
2011-08-09 18:03:54 -0300

    refatorado nome da propriedade localDir para LOCAL_DIR

--- src/test/java/br/com/zup/file/test/FileCleannerTest.java
+++ src/test/java/br/com/zup/file/test/FileCleannerTest.java
@@ -19,15 +19,15 @@ public class FileCleannerTest {
 
 	private static final String LOCATION_CLASSES = "/target/test-classes/unit/orm-cleanner-test/src/main/java/br/com/ctbc/model/";
 
-	private static final String localDir;
+	private static final String LOCAL_DIR;
 
 	static {
-		localDir = System.getProperty("user.dir");
+		LOCAL_DIR = System.getProperty("user.dir");
 	}
 
 	@Test
 	public void shouldFileIsEntity() throws IOException {
-		File entityFile = new File(localDir + LOCATION_CLASSES + ENTITY_CLASS);
+		File entityFile = new File(LOCAL_DIR + LOCATION_CLASSES + ENTITY_CLASS);
 
 		FileCleanner cleanner = new FileCleanner(entityFile);
 		assertTrue(cleanner.isEntity());
@@ -35,7 +35,7 @@ public class FileCleannerTest {
 
 	@Test
 	public void shouldFileNotIsEntity() throws IOException {
-		File noEntityFile = new File(localDir + LOCATION_CLASSES + NO_ENTITY_CLASS);
+		File noEntityFile = new File(LOCAL_DIR + LOCATION_CLASSES + NO_ENTITY_CLASS);
 
 		FileCleanner cleanner = new FileCleanner(noEntityFile);
 		assertFalse(cleanner.isEntity());
@@ -43,9 +43,9 @@ public class FileCleannerTest {
 
 	@Test
 	public void sholdClean() throws Exception {
-		File entityFile = new File(localDir + LOCATION_CLASSES + ENTITY_CLASS);
+		File entityFile = new File(LOCAL_DIR + LOCATION_CLASSES + ENTITY_CLASS);
 		
-		File noEntityFile = new File(localDir + LOCATION_CLASSES + NO_ENTITY_CLASS);
+		File noEntityFile = new File(LOCAL_DIR + LOCATION_CLASSES + NO_ENTITY_CLASS);
 		FileReader noEntityReader = new FileReader(noEntityFile);
 		BufferedReader noEntityBuffer = new BufferedReader(noEntityReader);
 		StringBuilder noEntity = new StringBuilder();
