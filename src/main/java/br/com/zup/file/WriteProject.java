22d2fcdab3a8e887109af014a01be16a33b646ef
Abner Carleto <abnercarleto@ymail.com>
2011-08-11 15:47:15 -0300

    removido metodos que ja não são necessarios

--- src/main/java/br/com/zup/file/WriteProject.java
+++ src/main/java/br/com/zup/file/WriteProject.java
@@ -6,49 +6,19 @@ import java.io.IOException;
 import java.util.List;
 
 import br.com.zup.pomxml.Dependency;
-import br.com.zup.pomxml.PomXml;
 
 public class WriteProject {
 	
-	private String artifactId;
-	private String artifactGroupId;
-	private String version;
-	private List<Dependency> dependencies;
-	
 	private File location;
 	
-	private FileWriter pom;
-	private File projectDirectory;
-	
-	
-
-	public WriteProject(String artifactId, String artifactGroupId, String version, List<Dependency> dependencies, File location) throws IOException {
+	public WriteProject(File location) throws IOException {
 		super();
-		this.artifactId = artifactId;
-		this.artifactGroupId = artifactGroupId;
-		this.version = version;
-		this.dependencies = dependencies;
 		this.location = location;
-		
-		this.projectDirectory = new File(location, artifactId);
 	}
-
-
 
 	public void generateProject() throws IOException {
 		writeProjectDirectory();
-		writePom();
 	}
-
-
-
-	private void writePom() throws IOException {
-		this.pom = new FileWriter(new File(this.projectDirectory, "pom.xml"));
-		pom.write("");
-		pom.close();
-	}
-
-
 
 	private void writeProjectDirectory() throws IOException {
 		String error = "Directory %s, not can be write";
@@ -56,14 +26,6 @@ public class WriteProject {
 			throw new IOException(String.format(error, location.toString()));
 		if (!projectDirectory.mkdir())
 			throw new IOException(String.format(error, projectDirectory.toString()));
-	}
-
-
-
-	public PomXml generatePom() {
-		PomXml pom = new PomXml(artifactGroupId, artifactId, version);
-		pom.setDependencies(dependencies);
-		return pom;
 	}
 
 }
--- src/test/java/br/com/zup/file/test/WriteProjectTest.java
+++ src/test/java/br/com/zup/file/test/WriteProjectTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.transform.stream.StreamSource;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.castor.CastorMarshaller;
 
 import br.com.zup.file.WriteProject;
 import br.com.zup.pomxml.Dependency;
@@ -26,9 +31,11 @@ public class WriteProjectTest {
 	private static final PomXml POM_TEST;
 	private static final File LOCATION;
 	private static final String VERSION = "1.0.0-SNAPSHOT";
-
+	
 	private static final Dependency GUAVA = new Dependency("com.google.guava", "guava");
 	private static final Dependency JUNIT = new Dependency("junit", "junit");
+	
+	private static final List<Dependency> DEPENDENCIES;
 
 	static {
 		LOCAL_DIR = System.getProperty("user.dir");
@@ -38,6 +45,10 @@ public class WriteProjectTest {
 		POM_TEST = new PomXml(ARTIFACT_GROUP_ID, ARTIFACT_ID, VERSION);
 		POM_TEST.addDependency(GUAVA);
 		POM_TEST.addDependency(JUNIT);
+		
+		DEPENDENCIES = new ArrayList<Dependency>();
+		DEPENDENCIES.add(GUAVA);
+		DEPENDENCIES.add(JUNIT);
 	}
 
 	@Before
@@ -60,7 +71,7 @@ public class WriteProjectTest {
 
 	@Test
 	public void shoulGenerateProject() throws Exception {
-		WriteProject writeFiles = new WriteProject(ARTIFACT_ID, ARTIFACT_GROUP_ID, VERSION, null, LOCATION);
+		WriteProject writeFiles = new WriteProject(LOCATION);
 		writeFiles.generateProject();
 
 		File project = new File(LOCAL_DIR + BASE_LOCATION + ARTIFACT_ID);
@@ -72,13 +83,4 @@ public class WriteProjectTest {
 		assertTrue(pom.isFile());
 	}
 
-	@Test
-	public void shouldPomXml() throws Exception {
-		List<Dependency> dependencies = new ArrayList<Dependency>();
-		dependencies.add(GUAVA);
-		dependencies.add(JUNIT);
-		WriteProject writeFiles = new WriteProject(ARTIFACT_ID, ARTIFACT_GROUP_ID, VERSION, dependencies, LOCATION);
-		PomXml pomXml = writeFiles.generatePom();
-		assertEquals(POM_TEST, pomXml);
-	}
 }
