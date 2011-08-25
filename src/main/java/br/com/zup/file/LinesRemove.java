082f2fa524e6c4206da12ada2ddbe0ea2567c1f7
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2011-08-25 14:11:00 -0300

    Adicionando mais regexs: GeneratedValue e SequenceGenerator

--- src/main/java/br/com/zup/file/LinesRemove.java
+++ src/main/java/br/com/zup/file/LinesRemove.java
@@ -31,18 +31,7 @@ package br.com.zup.file;
 public enum LinesRemove {
 	
 	// IMPORTS
-	IMPORT_ENTITY("import javax.persistence.Entity;"),
-	IMPORT_TABLE("import javax.persistence.Table;"),
-	IMPORT_COLUMN("import javax.persistence.Column;"),
-	IMPORT_ID("import javax.persistence.Id;"),
-	IMPORT_FETCH_TYPE("import javax.persistence.FetchType;"),
-	IMPORT_ONE_TO_ONE("import javax.persistence.OneToOne;"),
-	IMPORT_JOIN_COLUMN("import javax.persistence.JoinTable;"),
-	IMPORT_JOIN_TABLE("import javax.persistence.JoinColumn;"),
-	IMPORT_MANY_TO_ONE("import javax.persistence.ManyToOne;"),
-	IMPORT_TRANSIENT("import javax.persistence.Transient;"),
-	IMPORT_ONE_TO_MANY("import javax.persistence.OneToMany;"),
-	IMPORT_MANY_TO_MANY("import javax.persistence.ManyToMany;"),
+	IMPORT_JAVAX_PERSISTENCE("import javax.persistence.(.+?);"),
 	
 	// ANNOTATIONS
 	ANNOTATION_ENTITY("@Entity(\\s*\\(.+?\\))|@Entity"),
@@ -55,7 +44,9 @@ public enum LinesRemove {
 	ANNOTATION_MANY_TO_ONE("@ManyToOne(\\s*\\(.+?\\))|@ManyToOne"),
 	ANNOTATION_TRANSIENT("@Transient(\\s*\\(.+?\\))|@Transient"),
 	ANNOTATION_ONE_TO_MANY("@OneToMany(\\s*\\(.+?\\))|@OneToMany"),
-	ANNOTATION_MANY_TO_MANY("@ManyToMany(\\s*\\(.+?\\))|@ManyToMany");
+	ANNOTATION_MANY_TO_MANY("@ManyToMany(\\s*\\(.+?\\))|@ManyToMany"),
+	ANNOTATION_GENERATED_VALUE("@GeneratedValue(\\s*\\(.+?\\))|@GeneratedValue"),
+	ANNOTATION_SEQUENCE_GENERATOR("@SequenceGenerator(\\s*\\(.+?\\))|@SequenceGenerator");
 
 	private final String lineRegex;
 
