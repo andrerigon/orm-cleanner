4911555a7282c9c0291c5414459b23098ac08449
André Ribeiro Gonçalves <andre.rigon@gmail.com>
2011-09-13 13:56:01 -0300

    formatter test case fix

--- src/test/java/br/com/zup/file/test/FileFormatterTest.java
+++ src/test/java/br/com/zup/file/test/FileFormatterTest.java
@@ -11,12 +11,14 @@ import br.com.zup.file.JavaCodeFormatter;
 
 public class FileFormatterTest {
 
+	private static final String UNFORMATTED_CODE = "public enum X {A,	B,	C,	D,	E,	F}";
+	
 	private final String FORMATTED_CODE = readFile("formatted_enum.txt");
 
 	@Test
 	public void should_format_code() {
 
-		assertEquals(FORMATTED_CODE, new JavaCodeFormatter().format(FORMATTED_CODE));
+		assertEquals(FORMATTED_CODE, new JavaCodeFormatter().format(UNFORMATTED_CODE));
 	}
 
 	private String readFile(String code) {
